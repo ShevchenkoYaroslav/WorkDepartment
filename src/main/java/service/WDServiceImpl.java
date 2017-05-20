17e09427b3bb74261d26118984e1b787f4e416ad
Yaroslav Shevchenko <valsorya94@gmail.com>
2017-05-20 23:18:51 +0300

    need fix

--- src/main/java/service/WDServiceImpl.java
+++ src/main/java/service/WDServiceImpl.java
@@ -39,6 +39,8 @@ public class WDServiceImpl implements WDService {
 
         executeSQL(sql);
 
+
+        //TODO need fix
         try {
             Statement statement = connection.createStatement();
             ResultSet rs = statement.executeQuery(sql);
